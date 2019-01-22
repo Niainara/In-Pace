34848cd37c6edfaae6fb7b0f705036359c9f7e7f
cormobro <bonaet.felix@gmail.com>
2019-01-22 13:31:13 +0100

    ajout premiere page

---
+++ front/inpace/src/pages/accueil/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class Accueil extends Component {
+  render() {
+    return (
+      <div className="index">
+      </div>
+    );
+  }
+}
+
+export default Accueil;
+
