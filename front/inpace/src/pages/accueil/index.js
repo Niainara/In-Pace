acc04772c6a8812b150cb3c8ed3532daeac65758
cormobro <bonaet.felix@gmail.com>
2019-01-23 14:55:13 +0100

    Creation firstpage

--- front/inpace/src/pages/accueil/index.js
+++ front/inpace/src/pages/accueil/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './index.css';
+import Logo from '../../assets/logo.png';
+import Background from '../../assets/Fleur.jpg';
 
 class Accueil extends Component {
 	constructor(props) {
@@ -10,7 +12,27 @@ class Accueil extends Component {
 
 	render() {
 		return (
-			<div className="index">Salut toi</div>
+			<div className="index">
+				<div className="mainpage">
+					<div className="mainpage__card">
+						<img src={Logo} alt="logo" height="50px" width="200px" className="mainpage__card__logo" />
+						<div className="mainpage__card__text">
+							CONSULTEZ LES POMPES FUNEBRES DISPONIBLES EN TOUTE CONFIANCE
+						</div>
+					</div>
+				</div>
+				<div className="nextpage">
+					<div className="nextpage__text">
+						FAITES NOUS CONFIANCE ET COMMENCEZ A CHERCHER LES SERVICES QUI VOUS CONVIENNENT AFIN DE PREPARER LES FUNERAILLES DANS LES MEILLEURES CONDITIONS
+					</div>
+					<div className="nextpage__form">
+						form
+					</div>
+					<div className="nextpage__footer">
+						footer
+					</div>
+				</div>
+			</div>
 		);
 	}
 }
