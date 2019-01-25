b758809f213e8d9671d81db11368c775a4794d7e
Anja Razafitrimo <anjazarafitrimo@msn.com>
2019-01-25 11:26:42 +0100

    creation des components toolbar et page base

---
+++ front/inpace/src/components/Toolbar/toolbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './toolbar.css';
+
+const Toolbar = (
+  <header className="toolbar">
+    <h2>
+        IN PACE
+    </h2>
+    <ul>
+      <li>
+        accueil
+      </li>
+      <li>
+        accueil
+      </li>
+      <li>
+        accueil
+      </li>
+      <li>
+        accueil
+      </li>
+    </ul>
+  </header>
+);
+export default Toolbar;
