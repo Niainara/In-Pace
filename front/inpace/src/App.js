3ac7dc656ba7703abdb06e03e5c65b7af6b5b81d
Niainara <43339656+Niainara@users.noreply.github.com>
2019-02-05 10:45:24 +0100

    Revert "Form"

--- front/inpace/src/App.js
+++ front/inpace/src/App.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Accueil from './pages/accueil/index';
 
 const App = () => (
 	<div>
-		<BrowserRouter>
-			<Switch>
-				<Route path={['/', '/accueil']} exact component={Accueil} />
-			</Switch>
-		</BrowserRouter>
+		<Accueil />
 	</div>);
 
 export default App;
--- front/inpace/src/index.js
+++ front/inpace/src/index.js
@@ -4,23 +4,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
-import { reducer as formReducer } from 'redux-form';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import devToolsEnhancer from 'remote-redux-devtools';
 
-//On combine les reducers pour les passer en une fois au store
-const rootReducer = combineReducers({
-	form: formReducer
-})
 
-//création du store auquel on passe nos reducers ainsi qu'une fonction permettant d'utiliser l'utilitaire redux sur navigateur
-const store = createStore(rootReducer, devToolsEnhancer());
-
-//On encapsule notre App dans le provider redux ainsi que le provider material UI
-ReactDOM.render(<Provider store={store}><MuiThemeProvider muiTheme={getMuiTheme()}><App /></MuiThemeProvider></Provider>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
