89bc7b32a715a24f8b4573fec55e070d8f8d796f
Anja Razafitrimo <anjazarafitrimo@msn.com>
2019-02-18 11:29:41 +0100

    Page options finie, mais eslint material ui à vérifier

--- front/inpace/src/components/Options/Choice-options/index.js
+++ front/inpace/src/components/Options/Choice-options/index.js
@@ -2,74 +2,183 @@ import React from 'react';
 import './index.css';
 import { NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import Select from 'react-select';
 import { withStyles } from '@material-ui/core/styles';
-import green from '@material-ui/core/colors/green';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import ExpansionPanel from '@material-ui/core/ExpansionPanel';
-import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
-import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import Typography from '@material-ui/core/Typography';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import InputLabel from '@material-ui/core/InputLabel';
+import NoSsr from '@material-ui/core/NoSsr';
+import TextField from '@material-ui/core/TextField';
+import Paper from '@material-ui/core/Paper';
+import Chip from '@material-ui/core/Chip';
 import MenuItem from '@material-ui/core/MenuItem';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
-import InputBase from '@material-ui/core/InputBase';
+import CancelIcon from '@material-ui/icons/Cancel';
+import { emphasize } from '@material-ui/core/styles/colorManipulator';
 
-const BootstrapInput = withStyles(theme => ({
-  root: {
-    'label + &': {
-      marginTop: theme.spacing.unit * 3,
-    },
-  },
-  input: {
-    borderRadius: 4,
-    position: 'relative',
-    backgroundColor: theme.palette.background.paper,
-    border: '1px solid #ced4da',
-    fontSize: 16,
-    width: 'auto',
-    padding: '10px 26px 10px 12px',
-    transition: theme.transitions.create(['border-color', 'box-shadow']),
-    // Use the system font instead of the default Roboto font.
-    fontFamily: [
-      '-apple-system',
-      'BlinkMacSystemFont',
-      '"Segoe UI"',
-      'Roboto',
-      '"Helvetica Neue"',
-      'Arial',
-      'sans-serif',
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"',
-    ].join(','),
-    '&:focus': {
-      borderRadius: 4,
-      borderColor: '#80bdff',
-      boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)',
-    },
-  },
-}))(InputBase);
+const suggestions = [
+  { label: '1' },
+  { label: '2' },
+  { label: '3' },
+  { label: '4' },
+  { label: '5' },
+  { label: '6' },
+  { label: '7' },
+  { label: '8' },
+  { label: '9' },
+  { label: '10' },
+  { label: '11' },
+  { label: '12' },
+  { label: '13' },
+  { label: '14' },
+  { label: '15' },
+  { label: '16' },
+  { label: '17' },
+  { label: '18' },
+  { label: '20' },
+  { label: '21' },
+].map(suggestion => ({
+  value: suggestion.label,
+  label: suggestion.label,
+}));
 
 const styles = theme => ({
   root: {
-    color: green[600],
-    '&$checked': {
-      color: green[500],
-      display: 'flex',
-      flexWrap: 'wrap',
-    },
+    flexGrow: 1,
+    height: 250,
   },
-  checked: {},
-  margin: {
-    margin: theme.spacing.unit,
+  input: {
+    display: 'flex',
+    padding: 0,
   },
-  bootstrapFormLabel: {
-    fontSize: 18,
+  valueContainer: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    flex: 1,
+    alignItems: 'center',
+    overflow: 'hidden',
+  },
+  chip: {
+    margin: `${theme.spacing.unit / 2}px ${theme.spacing.unit / 4}px`,
+  },
+  chipFocused: {
+    backgroundColor: emphasize(
+      theme.palette.type === 'light' ? theme.palette.grey[300] : theme.palette.grey[700],
+      0.08,
+    ),
+  },
+  noOptionsMessage: {
+    padding: `${theme.spacing.unit}px ${theme.spacing.unit * 2}px`,
+  },
+  singleValue: {
+    fontSize: 16,
+  },
+  placeholder: {
+    position: 'absolute',
+    left: 2,
+    fontSize: 16,
+  },
+  paper: {
+    position: 'absolute',
+    zIndex: 1,
+    marginTop: theme.spacing.unit,
+    left: 0,
+    right: 0,
+  },
+  divider: {
+    height: theme.spacing.unit * 2,
   },
 });
+
+function NoOptionsMessage(props) {
+  return (
+    <Typography
+      color="textSecondary"
+      className={props.selectProps.classes.noOptionsMessage}
+      {...props.innerProps}
+    >
+      {props.children}
+    </Typography>
+  );
+}
+
+function inputComponent({ inputRef, ...props }) {
+  return <div ref={inputRef} {...props} />;
+}
+
+function Control(props) {
+  return (
+    <TextField
+      fullWidth
+      InputProps={{
+        inputComponent,
+        inputProps: {
+          className: props.selectProps.classes.input,
+          inputRef: props.innerRef,
+          children: props.children,
+          ...props.innerProps,
+        },
+      }}
+      {...props.selectProps.textFieldProps}
+    />
+  );
+}
+
+function Option(props) {
+  return (
+    <MenuItem
+      divRef={props.innerRef}
+      selected={props.isFocused}
+      component="div"
+      style={{
+        fontWeight: props.isSelected ? 500 : 400,
+      }}
+      {...props.innerProps}
+    >
+      {props.children}
+    </MenuItem>
+  );
+}
+
+function Placeholder(props) {
+  return (
+    <Typography
+      color="textSecondary"
+      className={props.selectProps.classes.placeholder}
+      {...props.innerProps}
+    >
+      {props.children}
+    </Typography>
+  );
+}
+
+function SingleValue(props) {
+  return (
+    <Typography className={props.selectProps.classes.singleValue} {...props.innerProps}>
+      {props.children}
+    </Typography>
+  );
+}
+
+function ValueContainer(props) {
+  return <div className={props.selectProps.classes.valueContainer}>{props.children}</div>;
+}
+
+function Menu(props) {
+  return (
+    <Paper square className={props.selectProps.classes.paper} {...props.innerProps}>
+      {props.children}
+    </Paper>
+  );
+}
+
+const components = {
+  Control,
+  Menu,
+  NoOptionsMessage,
+  Option,
+  Placeholder,
+  SingleValue,
+  ValueContainer,
+};
 
 class ChoiceOptions extends React.Component {
   constructor(props) {
@@ -77,136 +186,132 @@ class ChoiceOptions extends React.Component {
     this.state = {
       checkedBox: true,
       age: '',
+      sobre: null,
+      standard: null,
+      luxe: null,
+      ceremonieOui: false,
+      ceremonieNon: false,
+      ceremonieJeNeSaisPas: false,
+      urneOui: false,
+      urneNon: false,
+      urneJeNeSaisPas: false,
     };
   }
 
-  handleChange = name => event => {
-    this.setState({ [name]: event.target.checked });
-  };
-
-  handleChangeAge = event => {
-    this.setState({ age: event.target.value });
+  handleChange = name => value => {
+    this.setState({
+      [name]: value,
+    });
   };
 
   render() {
-    const { classes } = this.props;
+    const { classes, theme } = this.props;
+    const selectStyles = {
+      input: base => ({
+        ...base,
+        color: theme.palette.text.primary,
+        '& input': {
+          font: 'inherit',
+        },
+      }),
+    };
+    const { ceremonieOui } = this.state;
+    const { ceremonieNon } = this.state;
+    const { ceremonieJeNeSaisPas } = this.state;
+    const { urneOui } = this.state;
+    const { urneNon } = this.state;
+    const { urneJeNeSaisPas } = this.state;
+
     return (
       <React.Fragment>
         <div className="container__options">
-          <div className="div-btn__options">
+          <div className="div-btn__options div-btn__options--background">
             <div className="div-btn__options__section">
-              <div className="div-btn__options__section__header"> Cérémonie </div>
               <div className="div-btn__options__section__description">
-                <FormControlLabel
-                  control={<Checkbox checked={this.state.checkedBox} onChange={this.handleChange('checkedBox')} value="checkedBox" classes={{ root: classes.root, checked: classes.checked }} />}
-                  label="Oui"
-                />
-                <FormControlLabel
-                  control={<Checkbox checked={this.state.checkedBox} onChange={this.handleChange('checkedBox')} value="checkedBox" classes={{ root: classes.root, checked: classes.checked }} />}
-                  label="Je ne sais pas"
-                />
+                <div className="div-btn__options__section__header"> Cérémonie </div>
+                <div className={ceremonieOui ? 'div-btn__options__section__description__item--change  height__change' : 'div-btn__options__section__description__item  height__change'} onClick={() => this.setState({ ceremonieOui: !ceremonieOui, ceremonieNon: false, ceremonieJeNeSaisPas: false })} onKeyDown={this.handleKeyDown} role="button" tabIndex={0}>Oui</div>
+                <div className={ceremonieNon ? 'div-btn__options__section__description__item--change  height__change' : 'div-btn__options__section__description__item  height__change'} onClick={() => this.setState({ ceremonieNon: !ceremonieNon, ceremonieOui: false, ceremonieJeNeSaisPas: false })} onKeyDown={this.handleKeyDown} role="button" tabIndex={0}>Non</div>
+                <div className={ceremonieJeNeSaisPas ? 'div-btn__options__section__description__item--change  height__change' : 'div-btn__options__section__description__item  height__change'} onClick={() => this.setState({ ceremonieJeNeSaisPas: !ceremonieJeNeSaisPas, ceremonieNon: false, ceremonieOui: false })} onKeyDown={this.handleKeyDown} role="button" tabIndex={0}>Je ne sais pas</div>
               </div>
             </div>
             <div className="div-btn__options__section">
-              <div className="div-btn__options__section__header"> Fleurs </div>
               <div className="div-btn__options__section__description">
-                <ExpansionPanel>
-                  <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-                    <Typography className={classes.heading}>Expansion Panel 1</Typography>
-                    <FormControl className={classes.margin}>
-                      <InputLabel htmlFor="age-customized-select" className={classes.bootstrapFormLabel}>
-                      Nbre
-                      </InputLabel>
-                      <Select
-                        value={this.state.age}
-                        onChange={this.handleChangeAge}
-                        input={<BootstrapInput name="age" id="age-customized-select" />}
-                      >
-                        <MenuItem value="">
-                          <em>0</em>
-                        </MenuItem>
-                        <MenuItem value={1}>1</MenuItem>
-                        <MenuItem value={2}>2</MenuItem>
-                        <MenuItem value={3}>3</MenuItem>
-                      </Select>
-                    </FormControl>
-                  </ExpansionPanelSummary>
-                  <ExpansionPanelDetails>
-                    <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-            sit amet blandit leo lobortis eget.
-                    </Typography>
-                  </ExpansionPanelDetails>
-                </ExpansionPanel>
-                <ExpansionPanel>
-                  <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-                    <Typography className={classes.heading}>Expansion Panel 2</Typography>
-                    <FormControl className={classes.margin}>
-                      <InputLabel htmlFor="age-customized-select" className={classes.bootstrapFormLabel}>
-            Nbre
-                      </InputLabel>
-                      <Select
-                        value={this.state.age}
-                        onChange={this.handleChangeAge}
-                        input={<BootstrapInput name="age" id="age-customized-select" />}
-                      >
-                        <MenuItem value="">
-                          <em>0</em>
-                        </MenuItem>
-                        <MenuItem value={1}>1</MenuItem>
-                        <MenuItem value={2}>2</MenuItem>
-                        <MenuItem value={3}>3</MenuItem>
-                      </Select>
-                    </FormControl>
-                  </ExpansionPanelSummary>
-                  <ExpansionPanelDetails>
-                    <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-            sit amet blandit leo lobortis eget.
-                    </Typography>
-                  </ExpansionPanelDetails>
-                </ExpansionPanel>
-                <ExpansionPanel>
-                  <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-                    <Typography className={classes.heading}>Disabled Expansion Panel</Typography>
-                    <FormControl className={classes.margin}>
-                      <InputLabel htmlFor="age-customized-select" className={classes.bootstrapFormLabel}>
-            Nbre
-                      </InputLabel>
-                      <Select
-                        value={this.state.age}
-                        onChange={this.handleChangeAge}
-                        input={<BootstrapInput name="age" id="age-customized-select" />}
-                      >
-                        <MenuItem value="">
-                          <em>0</em>
-                        </MenuItem>
-                        <MenuItem value={1}>1</MenuItem>
-                        <MenuItem value={2}>2</MenuItem>
-                        <MenuItem value={3}>3</MenuItem>
-                      </Select>
-                    </FormControl>
-                  </ExpansionPanelSummary>
-                </ExpansionPanel>
+                <div className="div-btn__options__section__header"> Fleurs </div>
+                <div className="div-btn__options__section__description__item">
+                  <img className="div-btn__options__section__description__item__photo" src="https://via.placeholder.com/150x120" alt="pic" />
+                  <div className="div-btn__options__section__description__item__selection">
+                    <p className="div-btn__options__section__description__item__text">Sobre</p>
+                    <div className={classes.root}>
+                      <NoSsr>
+                        <Select
+                          classes={classes}
+                          styles={selectStyles}
+                          options={suggestions}
+                          components={components}
+                          value={this.state.sobre}
+                          onChange={this.handleChange('sobre')}
+                          placeholder="0"
+                          isClearable
+                        />
+                      </NoSsr>
+                    </div>
+                  </div>
+                </div>
+                <div className="div-btn__options__section__description__item">
+                  <img className="div-btn__options__section__description__item__photo" src="https://via.placeholder.com/150x120" alt="pic" />
+                  <div className="div-btn__options__section__description__item__selection">
+                    <p className="div-btn__options__section__description__item__text">Standard</p>
+                    <div className={classes.root}>
+                      <NoSsr>
+                        <Select
+                          classes={classes}
+                          styles={selectStyles}
+                          options={suggestions}
+                          components={components}
+                          value={this.state.standard}
+                          onChange={this.handleChange('standard')}
+                          placeholder="0"
+                          isClearable
+                          className="selector"
+                        />
+                      </NoSsr>
+                    </div>
+                  </div>
+                </div>
+                <div className="div-btn__options__section__description__item">
+                  <img className="div-btn__options__section__description__item__photo" src="https://via.placeholder.com/150x120" alt="pic" />
+                  <div className="div-btn__options__section__description__item__selection">
+                    <p className="div-btn__options__section__description__item__text">Luxe</p>
+                    <div className={classes.root}>
+                      <NoSsr>
+                        <Select
+                          classes={classes}
+                          styles={selectStyles}
+                          options={suggestions}
+                          components={components}
+                          value={this.state.luxe}
+                          onChange={this.handleChange('luxe')}
+                          placeholder="0"
+                          isClearable
+                        />
+                      </NoSsr>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
             <div className="div-btn__options__section">
-              <div className=" div-btn__options__section__header"> Urne </div>
               <div className="div-btn__options__section__description">
-                <FormControlLabel
-                  control={<Checkbox checked={this.setState.checkedBox} onChange={this.handleChange('checkedBox')} value="checkedBox" classes={{ root: classes.root, checked: classes.checked }} />}
-                  label="Oui"
-                />
-                <FormControlLabel
-                  control={<Checkbox checked={this.setState.checkedBox} onChange={this.handleChange('checkedBox')} value="checkedBox" classes={{ root: classes.root, checked: classes.checked }} />}
-                  label="Je ne sais pas"
-                />
+                <div className=" div-btn__options__section__header"> Urne </div>
+                <div className={urneOui ? 'div-btn__options__section__description__item--change  height__change' : 'div-btn__options__section__description__item  height__change'} onClick={() => this.setState({ urneOui: !urneOui, urneNon: false, urneJeNeSaisPas: false })} onKeyDown={this.handleKeyDown} role="button" tabIndex={0}>Oui</div>
+                <div className={urneNon ? 'div-btn__options__section__description__item--change  height__change' : 'div-btn__options__section__description__item  height__change'} onClick={() => this.setState({ urneNon: !urneNon, urneOui: false, urneJeNeSaisPas: false })} onKeyDown={this.handleKeyDown} role="button" tabIndex={0}>Non</div>
+                <div className={urneJeNeSaisPas ? 'div-btn__options__section__description__item--change  height__change' : 'div-btn__options__section__description__item  height__change'} onClick={() => this.setState({ urneJeNeSaisPas: !urneJeNeSaisPas, urneNon: false, urneOui: false })} onKeyDown={this.handleKeyDown} role="button" tabIndex={0}>Je ne sais pas</div>
               </div>
             </div>
           </div>
           <div className="div-btn__options">
-            <NavLink to="/choix-de-cercueil" className="container__btn__options container__btn-precedent__options" type="div">Précédent</NavLink>
-            <NavLink to="/choix-des-options" className="container__btn__options container__btn-suivant__options" type="div">Suivant</NavLink>
+            <NavLink to="/choix-de-cercueil" className="container__btn__options container__btn-precedent__options">Précédent</NavLink>
+            <NavLink to="/choix-des-options" className="container__btn__options container__btn-suivant__options">Suivant</NavLink>
           </div>
         </div>
       </React.Fragment>
@@ -216,6 +321,7 @@ class ChoiceOptions extends React.Component {
 
 ChoiceOptions.propTypes = {
   classes: PropTypes.object.isRequired,
+  theme: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(ChoiceOptions);
+export default  withStyles(styles, { withTheme: true })(ChoiceOptions);
--- front/inpace/src/components/Options/Header-options/index.js
+++ front/inpace/src/components/Options/Header-options/index.js
@@ -20,7 +20,8 @@ class HeaderOptions extends React.Component {
           <li className="header__otpions__progress__item" />
         </ul>
         <h1 className="header__title__otpions">Les Options</h1>
-        <p className="header__description__otpions">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+        <p className="header__description__otpions">En fonction des volontés du défunt et/ou de vos désirs, il est possible de recourir à divers services et/ou produits additionnels dans le cadre des funérailles. Seuls les services fournis par l’ensemble des entrepreneurs de pompes funèbres sont proposés ci-dessous, à savoir l’organisation d’une cérémonie (laïque ou religieuse), la fourniture de fleurs ou d’une urne (en cas de crémation). Des explications détaillées relatives à chacun de ces services/produits peuvent être consultées en cliquant que le champ correspondant.</p>
+        <p className="header__description__otpions"> D’autres services et/ou produits additionnels (fournis par des prestataires externes en coopération avec les entrepreneurs de pompes funèbres) peuvent également être inclus dans l’organisation des funérailles : réception, avis nécrologique, faire-part/remerciements, rapatriement, etc. Il convient de se renseigner auprès des prestataires concernés ou de l’entrepreneur de pompes funèbres pour obtenir les différents prix applicables. Des prix indicatifs vous seront néanmoins indiqués lors du récapitulatif.</p>
       </div>
     );
   }
